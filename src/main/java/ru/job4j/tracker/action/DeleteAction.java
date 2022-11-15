33be7c805fa01ac21d4f275e536294c631ddc68c
dzhenetl <david.zhenetl@gmail.com>
2022-11-15 08:28:19 +0300

    2. Трекер SQL [#1734 #360673]

--- src/main/java/ru/job4j/tracker/action/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/action/DeleteAction.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker.action;
 
 import ru.job4j.tracker.output.Output;
-import ru.job4j.tracker.store.MemTracker;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.store.Store;
 
